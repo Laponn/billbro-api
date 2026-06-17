bc134cc7fb13cf6682f7300acd04c400deab1349
Laponn <naufal.sulthan.fakhry@gmail.com>
2026-06-17 17:26:22 +0700

    soft delete

--- app/Http/Controllers/Api/TagihanController.php
+++ app/Http/Controllers/Api/TagihanController.php
@@ -15,26 +15,38 @@ class TagihanController extends Controller
     }
 
     public function store(Request $request)
-{
-    $validated = $request->validate([
-        'namaTagihan' => 'required|string',
-        'totalTagihan' => 'required|numeric',
-        'jumlahOrang' => 'required|integer',
-        'pakaiPajak' => 'required|boolean',
-        'persentasePajak' => 'required|numeric',
-        'hasilPerOrang' => 'required|numeric',
-        'tanggalDibuat' => 'required|string',
-        'image' => 'required|image|mimes:jpeg,png,jpg|max:2048' // Validasi file gambar
-    ]);
+    {
+        $validated = $request->validate([
+            'namaTagihan' => 'required|string',
+            'totalTagihan' => 'required|numeric',
+            'jumlahOrang' => 'required|integer',
+            'pakaiPajak' => 'required|boolean',
+            'persentasePajak' => 'required|numeric',
+            'hasilPerOrang' => 'required|numeric',
+            'tanggalDibuat' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048' // Validasi file gambar
+        ]);
 
-    if ($request->hasFile('image')) {
-        $file = $request->file('image');
-        $fileName = time() . '_' . $file->getClientOriginalName();
-        $file->move(public_path('images'), $fileName);
-        $validated['imageId'] = pathinfo($fileName, PATHINFO_FILENAME); // Simpan nama file tanpa ekstensi ke imageId
+        
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $file->move(public_path('images'), $fileName);
+            $validated['imageId'] = pathinfo($fileName, PATHINFO_FILENAME); 
+        }
+
+        $tagihan = Tagihan::create($validated);
+        return response()->json(['status' => 'success', 'data' => $tagihan], 201);
     }
-
-    $tagihan = Tagihan::create($validated);
-    return response()->json(['status' => 'success', 'data' => $tagihan], 201);
-}
+    public function softDelete($id)
+    {
+        $tagihan = Tagihan::find($id);
+        if ($tagihan) {
+            $tagihan->isDeleted = true; 
+            $tagihan->save();
+            return response()->json(['status' => 'success', 'message' => 'Masuk ke Recycle Bin']);
+        }
+        return response()->json(['status' => 'error', 'message' => 'Data tidak ditemukan'], 404);
+    }
 }
